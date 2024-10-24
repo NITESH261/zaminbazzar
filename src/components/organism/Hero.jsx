33ca97253a0fb788681ddebe11ab657725f7a29a
Akash Lakade <akashlakade130@gmail.com>
2024-10-24 21:02:22 +0530

    refactor: hero ui refactored

--- src/components/organism/Hero.jsx
+++ src/components/organism/Hero.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import Background from '../atoms/Background';
 
 const Hero = () => {
-  return (
-    <div className='w-full h-96 flex items-center justify-center relative max-w-full'>
-      <Background />
-      <div className="absolute flex items-center justify-center h-full w-full bg-black bg-opacity-50">
-        <h1 className="text-white  w-[800px] text-6xl break-normal text-center font-bold">The #1 site real estate professionals trust</h1>
-      </div>
-    </div>
-  );
+    return (
+        <>
+            <div className='w-full h-96 flex items-center justify-center relative'>
+                <Background />
+                <div className="absolute flex items-center justify-center h-full w-full bg-black bg-opacity-50">
+                    <h1 className="text-white w-full max-w-5xl text-3xl md:text-4xl lg:text-6xl  break-normal text-center font-bold">The #1 site real estate professionals trust</h1>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Hero
