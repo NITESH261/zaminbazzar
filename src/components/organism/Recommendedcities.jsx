d95be3665d400bdfa26916bb578d011e99794cb3
Akash Lakade <akashlakade130@gmail.com>
2024-10-26 20:39:16 +0530

    refactor: recommended cities ui card updated

--- src/components/organism/Recommendedcities.jsx
+++ src/components/organism/Recommendedcities.jsx
@@ -1,12 +1,10 @@
-
-import React from 'react'
-import Link from 'next/link';
-import Image from 'next/image';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Recommendedcities = () => {
     const cards = Array.from({ length: 4 }, (_, i) => i + 1);
     return (
-
         <>
             <div className="flex w-full">
                 <div className="flex flex-col gap-4 md:gap-6 w-full max-w-7xl mx-auto px-4 py-4 md:py-6 lg:py-8">
@@ -17,27 +15,47 @@ const Recommendedcities = () => {
                     </div>
                     <div className="flex w-full overflow-x-auto xl:overflow-x-hidden scrollbar-hide">
                         <div className="flex xl:grid xl:grid-cols-4 gap-4 w-fit xl:w-full">
-                            {
-                                cards.map(card =>
-                                    <Link key={card} href={"/"} className="w-80 flex-col rounded-lg xl:w-full group flex ">
-                                        <div className='relative aspect-[3/2] bg-cover rounded-lg flex w-full'>
-                                            <Image className='rounded-t-lg' src='/assets/house/staticmap.png' alt='house' fill />
+                            {cards.map((card) => (
+                                <Link
+                                    key={card}
+                                    href={"/"}
+                                    className="w-80 flex-col border border-neutral-200 rounded-lg xl:w-full group flex hover:shadow-lg"
+                                >
+                                    <div className="relative aspect-video bg-cover rounded-t-lg flex w-full overflow-hidden">
+                                        <Image
+                                            src="/assets/house/staticmap.png"
+                                            alt="house"
+                                            fill
+                                            className="rounded-t-lg group-hover:scale-110 transition-all object-cover"
+                                        />
+                                    </div>
+                                    <div className=" p-4 space-y-2">
+                                        <h2 className="text-lg font-bold">
+                                            Panama City Beach
+                                        </h2>
+                                        <div className="flex flex-col w-full">
+                                            <p className="text-gray-600 text-sm">
+                                                <b className="text-black">
+                                                    2,400
+                                                </b>{" "}
+                                                Listings for sale
+                                            </p>
+                                            <p className="text-gray-600 text-sm">
+                                                <b className="text-black">
+                                                    $475,000
+                                                </b>{" "}
+                                                Median Listing Home Price
+                                            </p>
                                         </div>
-                                        <div className="border rounded-lg p-4">
-
-                                            <h2 className="text-lg font-bold">Panama City Beach</h2>
-                                            <p className="text-gray-600">2,400 Listings for sale</p>
-                                            <p className="text-gray-600">$475,000 Median Listing Home Price</p>
-                                        </div>
-                                    </Link>
-                                )
-                            }
+                                    </div>
+                                </Link>
+                            ))}
                         </div>
                     </div>
                 </div>
-            </div >
+            </div>
         </>
-    )
-}
+    );
+};
 
-export default Recommendedcities
+export default Recommendedcities;
