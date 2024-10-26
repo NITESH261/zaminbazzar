debffb5c1b1211af569e41f67c86446af3fd16a2
Akash Lakade <akashlakade130@gmail.com>
2024-10-26 16:49:21 +0530

    refactor: searchbar mobile view changed

--- src/components/organism/Hero.jsx
+++ src/components/organism/Hero.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import Background from '../atoms/Background';
 import { ChevronDown } from 'lucide-react';
-import { Search } from 'lucide-react';
-import { Button } from '../ui/button';
 import { LocateIcon } from 'lucide-react';
 import { MicIcon } from 'lucide-react';
-import { Input } from '../ui/input';
 import SearchBar from '../molecules/SearchBar';
 
 const Hero = () => {
@@ -15,8 +12,8 @@ const Hero = () => {
                 <Background />
                 <div className="absolute flex flex-col gap-6 items-center justify-center h-full w-full bg-black/20">
                     <h1 className="font-galanoGrotesque text-white w-full max-w-3xl text-3xl md:text-4xl lg:text-5xl  break-normal text-center font-bold">The #1 site real estate professionals trust</h1>
-                    <div className="flex absolute top-[80%] shadow-md flex-col divide-y w-full max-w-4xl bg-white rounded-2xl border border-gray-200">
-                        <div className="flex w-full text-sm">
+                    <div className="flex absolute top-[80%] shadow-md flex-col divide-y w-full max-w-4xl bg-white rounded-2xl md:border md:border-gray-200">
+                        <div className="hidden md:flex w-full text-sm">
                             <ul className="grid grid-cols-7 h-14 w-full">
                                 <li className='flex w-full items-center justify-center'>
                                     Buy
@@ -41,15 +38,15 @@ const Hero = () => {
                                 </li>
                             </ul>
                         </div>
-                        <div className="grid grid-cols-5 divide-x w-full h-14">
-                            <div className="flex w-full items-center justify-center">
+                        <div className="grid grid-cols-1 md:grid-cols-5 md:divide-x w-full h-14">
+                            <div className="hidden md:flex w-full items-center justify-center">
                                 <span className='text-sm'>All Residential</span>
                                 <ChevronDown className='size-6' />
                             </div>
-                            <div className="flex w-full px-5 items-center justify-center col-span-3">
+                            <div className="flex w-full px-5 items-center justify-center md:col-span-3">
                                 <SearchBar />
                             </div>
-                            <div className="flex w-full gap-2 items-center justify-center">
+                            <div className="hidden md:flex w-full gap-2 items-center justify-center">
                                 <span className='size-8 bg-blue-200 rounded-full items-center justify-center flex'>
                                     <LocateIcon className="size-5 text-blue-500" />
                                 </span>
