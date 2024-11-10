ef63dba8d59ae937f683c210bd77f1f55eac8efc
Akash09082001 <aakashl@letsupgrade.in>
2024-11-10 22:00:54 +0530

    refactor: news & blog section updated

--- src/components/organism/TrendingNews.jsx
+++ src/components/organism/TrendingNews.jsx
@@ -1,12 +1,16 @@
 import Link from "next/link";
-import { Badge } from "../ui/badge";
 
 const TrendingNews = () => {
     const cards = Array.from({ length: 4 }, (_, i) => i + 1);
     return (
         <>
             <div className="flex w-full bg-gray-200">
-                <div className="flex flex-col gap-4 md:gap-6 w-full max-w-7xl mx-auto px-4 py-4 md:py-6 lg:py-8 xl:py-10 xl:py-14">
+                <div className="flex flex-col gap-4 md:gap-6 w-full max-w-7xl mx-auto px-4 py-4 md:py-6 lg:py-8 xl:py-14">
+                    <div className="flex w-full">
+                        <h2 className="text-lg font-bold md:text-xl lg:text-2xl">
+                            News & Blogs
+                        </h2>
+                    </div>
                     <div className="flex w-full overflow-x-auto xl:overflow-x-hidden scrollbar-hide">
                         <div className="flex xl:grid xl:grid-cols-4 gap-4 w-fit xl:w-full">
                             {cards.map((card) => (
@@ -16,17 +20,15 @@ const TrendingNews = () => {
                                     className="w-80 flex flex-col bg-white rounded-lg xl:w-full group "
                                 >
                                     <div className="flex w-full relative aspect-[3/2] bg-cover rounded-lg bg-[url('/assets/house/house1.webp')]">
-                                        <div className="flex w-full p-4  bg-gradient-to-b from-black/40 to-transparent rounded-lg">
-                                            <Badge className="h-fit bg-blue-500 text-white">
-                                                Trend
-                                            </Badge>
-                                        </div>
                                     </div>
-                                    <div className="flex w-full p-4">
-                                        <p className="text-sm md:text-base text-neutral-700">
-                                            There Are Now 30 Renters for Every
-                                            Home for Sale in the U.S.
-                                        </p>
+                                    <div className="flex flex-col w-full gap-2 p-4">
+                                        <span className="text-sm md:text-base font-medium">
+                                            All You Need To Know About Godrej Emerald Thane West
+                                        </span>
+                                        <div className="flex text-xs text-neutral-500 justify-between w-full">
+                                            <span>By Lorem</span>
+                                            <span>17th Aug 18</span>
+                                        </div>
                                     </div>
                                 </Link>
                             ))}
