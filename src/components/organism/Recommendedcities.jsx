7b89940b7eb666fb3d8ab7995189f0ed28fb3a95
Akash09082001 <aakashl@letsupgrade.in>
2024-11-24 23:32:26 +0530

    refactor: ui & content updated

--- src/components/organism/Recommendedcities.jsx
+++ src/components/organism/Recommendedcities.jsx
@@ -6,7 +6,7 @@ const Recommendedcities = () => {
     return (
         <>
             <div className="flex w-full">
-                <div className="flex flex-col gap-4 md:gap-6 w-full max-w-7xl mx-auto px-4 py-4 md:py-6 lg:py-8 xl:py-10">
+                <div className="flex flex-col gap-4 md:gap-6 w-full max-w-7xl mx-auto px-4 py-6 md:py-8 lg:py-10 xl:py-12">
                     <div className="flex w-full">
                         <h2 className="text-lg font-bold md:text-xl lg:text-2xl">
                             Recommended Cities
