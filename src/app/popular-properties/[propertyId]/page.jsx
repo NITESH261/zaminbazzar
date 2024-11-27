e95c9c659dd070ab069ce8853ae459fd6beaa344
vinayak <vinayakarjun2018@gmail.com>
2024-11-27 22:02:37 +0530

    property about and similar property section

--- src/app/popular-properties/[propertyId]/page.jsx
+++ src/app/popular-properties/[propertyId]/page.jsx
@@ -1,7 +1,13 @@
+
 import Keyconnectivity from "@/components/atoms/Keyconnectivity"
+import SimilarProperty from "@/components/molecules/SimilarProperty"
 import BentoGridScroll from "@/components/molecules/BentoGridScroll"
 import EnquireForm from "@/components/molecules/EnquireForm"
 import { Button } from "@/components/ui/button"
+import Image from "next/image"
+
+
+
 
 const page = async ({ params }) => {
 
@@ -20,7 +26,7 @@ const page = async ({ params }) => {
                                     <span>Property For Sale</span>
                                 </div>
                                 <div className="flex gap-4 text-2xl md:text-3xl lg:text-4xl items-center w-full">
-                                    <strong className="">$806,950</strong>
+                                    <strong className="">₹8,06,950</strong>
                                     <span className="">|</span>
                                     <span className="text-xl md:text-2xl lg:text-3xl"> <span className="font-medium">2,894</span>{" "} sqft </span>
                                 </div>
@@ -81,6 +87,39 @@ const page = async ({ params }) => {
                     </div>
                 </div>
             </div>
+            <div>
+                <div className="flex w-full">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6 w-full max-w-7xl mx-auto px-4 py-4 md:py-6 lg:py-8 xl:py-20">
+                        <div className="flex flex-col space-y-4 md:space-y-6 w-full justify-center order-2">
+                            <div className="space-y-2 w-full">
+
+                                <h2 className="text-xl font-semibold md:text-2xl lg:text-3xl">
+                                    "Welcome to Zamin Wale, <br />
+                                    Experience eco-friendly solutions designed to enrich your daily journey!"
+                                </h2>
+                            </div>
+                            <p className="text-sm md:text-base">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque, minima temporibus recusandae voluptatem laudantium quis obcaecati suscipit, eos consequuntur ab nisi, fuga pariatur illo porro distinctio deserunt. Facere, est alias.
+                            </p>
+                            <div className="w-full space-x-4">
+                                <Button variant="outline" className="border-[#0078DB]">Know More</Button>
+                                <Button className="bg-[#0078DB]" >See Property Details</Button>
+                            </div>
+                        </div>
+                        <div className="flex w-full order-1">
+                            <div className="flex relative aspect-video w-full rounded-t-lg overflow-hidden">
+                                <Image
+                                    src="/assets/banner-img/about-img.webp"
+                                    alt="plots"
+                                    fill
+                                    className="rounded-lg object-cover object-center hover:scale-125 hover:transition-all hover:rotate-6"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <SimilarProperty />
         </div>
     )
 }
---
+++ src/components/molecules/SimilarProperty.jsx
@@ -0,0 +1,112 @@
+"use client"
+import React from 'react'
+import Link from "next/link"
+import { Button } from "../ui/button";
+import { ArrowRight } from "lucide-react"
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "../ui/carousel";
+import Autoplay from "embla-carousel-autoplay"
+import Image from "next/image"
+
+const Cards = [
+    {
+        img: "recomonded-property1.jpeg"
+    },
+    {
+        img: "recomonded-property2.jpeg"
+    },
+    {
+        img: "recomonded-property3.jpeg"
+    },
+    {
+        img: "recomonded-property4.jpeg"
+    },
+    {
+        img: "recomonded-property5.jpeg"
+    },
+    {
+        img: "recomonded-property6.jpeg"
+    },
+    {
+        img: "recomonded-property7.jpeg"
+    },
+    {
+        img: "recomonded-property8.jpeg"
+    },
+    {
+        img: "recomonded-property9.jpeg"
+    }
+]
+
+const SimilarProperty = () => {
+    return (
+        <>
+            <div className="flex w-full">
+                <div className="flex flex-col gap-4 md:gap-6 w-full max-w-7xl mx-auto px-4 py-6 md:py-8 lg:py-10 xl:py-12">
+                    <div className="flex w-full items-center justify-between">
+                        <h2 className="text-lg font-medium md:text-xl lg:text-2xl">
+                            Similar Property
+                        </h2>
+                        <Link href={"/"} className="flex items-center gap-2 hover:text-black text-[#0078DB] text-lg md:text-xl">
+                            <span className="hidden md:flex">See More Properties</span>
+                            <ArrowRight />
+                        </Link>
+                    </div>
+                    <div className="flex w-full">
+                        <Carousel
+                            plugins={[
+                                Autoplay({
+                                    delay: 3000,
+                                }),
+                            ]}
+                            opts={{
+                                align: "start",
+                                loop: true,
+                            }}
+                            className="flex w-full h-full"
+                        >
+                            <CarouselContent>
+                                {Cards.map((card, index) => (
+                                    <CarouselItem
+                                        key={`${index}-plot-img`}
+                                        className="sm:basis-1/2 lg:basis-1/3 xl:basis-1/4"
+                                    >
+                                        <div className="flex flex-col w-full border border-neutral-200 rounded-lg group hover:shadow-lg">
+                                            <div className="relative aspect-video bg-cover rounded-t-lg flex w-full overflow-hidden">
+                                                <Image
+                                                    src={`/assets/recommonded-property/${card.img}`}
+                                                    alt="house"
+                                                    fill
+                                                    className="rounded-t-lg group-hover:scale-110 transition-all object-cover"
+                                                />
+                                            </div>
+                                            <div className=" p-4 space-y-2">
+                                                <div className="w-full">
+                                                    <span className="text-sm">1 BHK Flat</span>
+                                                    <h2 className="text-lg font-medium">
+                                                        &#8377; 30 Lac | 1000 sqft
+                                                    </h2>
+                                                </div>
+                                                <div className="flex flex-col space-y-1 w-full">
+                                                    <p className="text-gray-600 text-sm">
+                                                        Vinay Nagar, Mumbai
+                                                    </p>
+                                                </div>
+                                                <Button className="rounded-full bg-[#0078DB]">
+                                                    View Details
+                                                </Button>
+                                            </div>
+                                        </div>
+                                    </CarouselItem>
+                                ))}
+                            </CarouselContent>
+                            <CarouselPrevious className="left-0 lg:-left-8" />
+                            <CarouselNext className="right-0 lg:-right-8" />
+                        </Carousel>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default SimilarProperty
