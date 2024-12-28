dcd9d42dd8e684bfd9959fee13cb5bc18f43a290
Akash09082001 <aakashl@letsupgrade.in>
2024-12-28 16:36:38 +0530

    refactor: footer updated

--- src/components/organism/Footer.jsx
+++ src/components/organism/Footer.jsx
@@ -1,65 +1,156 @@
-import { FaFacebookSquare, FaInstagram, FaLinkedin, FaYoutube } from "react-icons/fa";
-import { FaSquareXTwitter } from "react-icons/fa6";
+import Link from "next/link";
+import Logo from "../atoms/Logo";
 
 const Footer = () => {
     return (
-        <div className="bg-gray-100 text-gray-800 w-full">
-            <div className="container mx-auto py-8 px-4">
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                    <div>
-                        <h2 className="font-bold text-lg">About ZaminWale</h2>
-                        <p>As the largest platform connecting property buyers and sellers</p>
-                        <div className="mt-4">
-                            <h2 className="font-bold text-lg">More from our Network</h2>
-                            <p className="mt-2">Times Now &nbsp;&nbsp; ET Now</p>
-                            {/* <div className="flex space-x-2 mt-2">
-                                <a href="#"><img src="https://placehold.co/100x40" alt="Google Play" className="h-10" /></a>
-                                <a href="#"><img src="https://placehold.co/100x40" alt="App Store" className="h-10" /></a>
-                            </div> */}
-                            <div className="flex space-x-2 mt-4">
-                                <a href="#"><FaFacebookSquare className="fab fa-facebook-f text-blue-600 text-2xl" /></a>
-                                <a href="#"><FaSquareXTwitter className="fab fa-twitter text-blue-400 text-2xl" /></a>
-                                <a href="#"><FaLinkedin className="fab fa-linkedin-in text-blue-700 text-2xl" /></a>
-                                <a href="#"><FaInstagram className="fab fa-instagram text-pink-600 text-2xl" /></a>
-                                <a href="#"><FaYoutube className="fab fa-youtube text-red-600 text-2xl" /></a>
+        <>
+            <div className="flex flex-col w-full bg-blue-50 border-t border-neutral-200">
+                <div className="flex w-full max-w-7xl mx-auto px-4 py-6 md:py-8 lg:py-10 xl:py-12">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 lg:grid-cols-3">
+                        <div className="flex flex-col gap-4 md:gap-6 w-full">
+                            <span className="text-lg font-medium">Contact Info:</span>
+                            <div className="flex w-full flex-col gap-2 lg:gap-4">
+                                <p className="text-base">
+                                    G-Square Buisness Park,
+                                    3<sup>rd</sup> Floor Office no 303 & 304,
+                                    opposite of Sanpada Railway Station,
+                                    Navi Mumbai, Maharashatra 400703
+
+                                </p>
+                                <span>9555599299 / 9555599099</span>
+                                <span>info@zaminwale.com</span>
+                                <Logo />
+                            </div>
+                        </div>
+                        <div className="flex flex-col gap-4 w-full">
+                            <span className="text-lg font-medium">Company :</span>
+                            <ul className="flex w-full flex-col gap-2">
+                                {
+                                    NavItems[0].data.map((item, i) =>
+                                        <li key={`company-${i}`} className="w-fit">
+                                            <Link
+                                                href={item.link}
+                                                className="text-base"
+                                            >
+                                                {item.label}
+                                            </Link>
+                                        </li>
+                                    )
+                                }
+                            </ul>
+                        </div>
+                        <div className="flex flex-col gap-6 w-full">
+                            <div className="flex flex-col gap-4 w-full">
+                                <span className="text-lg font-medium">About Zaminwale :</span>
+                                <div className="flex w-full flex-col gap-2 lg:gap-4">
+                                    <p className="text-base">
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas dolore ullam id labore doloremque. Vero et quae tempora facilis illo aliquid, commodi nesciunt illum debitis asperiores qui odio, eius deleniti.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="flex flex-col gap-4 w-full">
+                                <span className="text-lg font-medium">Projects in Mumbai :</span>
+                                <ul className="flex w-full gap-1 flex-wrap">
+                                    {
+                                        NavItems[1].data.map((item, i) =>
+                                            <li key={`project-${i}`} className="w-fit">
+                                                <Link
+                                                    href={item.link}
+                                                    className="text-base"
+                                                >
+                                                    {item.label}
+                                                </Link>
+                                                <span className="px-2">|</span>
+                                            </li>
+                                        )
+                                    }
+                                </ul>
                             </div>
                         </div>
                     </div>
-                    <div>
-                        <h2 className="font-bold text-lg">Properties in India</h2>
-                        <p>Property in New Delhi | Property in Mumbai | Property in Chennai | Property in Pune | Property in Noida | Property in Gurgaon | Property in Bangalore | Property in Ahmedabad</p>
-                        <h2 className="font-bold text-lg mt-4">New Projects in India</h2>
-                        <p>New Projects in New Delhi | New Projects in Mumbai | New Projects in Chennai | New Projects in Pune | New Projects in Noida | New Projects in Gurgaon | New Projects in Bangalore | New Projects in Ahmedabad</p>
-                    </div>
+                </div>
+                <div className="flex w-full items-center bg-neutral-800 text-white justify-center py-6 lg:py-8 px-4">
+                    <p className="text-base md:text-lg text-center font-medium items-center justify-center">
+                        Copyright {new Date().getFullYear()} &copy; Zaminwale All Rights Reserved
+                    </p>
                 </div>
             </div>
-            <div className="bg-gray-200 py-4">
-                <div className="flex flex-wrap xl:w-fit mx-auto text-center">
-                    <a href="#" className="text-gray-600 mx-2">Sitemap</a>
-                    <a href="#" className="text-gray-600 mx-2">Terms & Conditions</a>
-                    <a href="#" className="text-gray-600 mx-2">Privacy Policy</a>
-                    <a href="#" className="text-gray-600 mx-2">Whistle Blower Policy</a>
-                    <a href="#" className="text-gray-600 mx-2">Blog</a>
-                    <a href="#" className="text-gray-600 mx-2">Careers</a>
-                    <a href="#" className="text-gray-600 mx-2">Testimonials</a>
-                    <a href="#" className="text-gray-600 mx-2">Unsubscribe</a>
-                    <a href="#" className="text-gray-600 mx-2">Help Center</a>
-                    <a href="#" className="text-gray-600 mx-2">Sales Enquiry</a>
-                    <a href="#" className="text-gray-600 mx-2">Buy Our Services</a>
-                </div>
-            </div>
-            <div className="bg-gray-300 py-4">
-                <div className="container mx-auto text-center text-sm text-gray-600">
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias rerum pariatur eos beatae quidem architecto, sit odit veniam illo mollitia, tempore qui. Atque qui sapiente minima dolorem. Aspernatur, accusamus odio.</p>
-                </div>
-            </div>
-            <div className="bg-gray-800 py-4">
-                <div className="container mx-auto text-center text-sm text-gray-400">
-                    <p>All trademarks, logos and names are properties of their respective owners. All Rights Reserved. © Copyright 2024 ZaminWale Pvt Limited.</p>
-                </div>
-            </div>
-        </div>
+        </>
     );
 };
 
 export default Footer;
+
+
+const NavItems = [
+    {
+        label: "Company",
+        data: [
+            {
+                label: "About Us",
+                link: "/about-us"
+            },
+            {
+                label: "Testimonials",
+                link: "/testimonials"
+            },
+            {
+                label: "Terms & Conditions",
+                link: "/terms-and-conditions"
+            },
+            {
+                label: "Privacy Policy",
+                link: "/privacy-policy"
+            },
+            {
+                label: "Carrers",
+                link: "/carrers"
+            },
+            {
+                label: "Carrers",
+                link: "/carrers"
+            },
+            {
+                label: "Blog",
+                link: "/blog"
+            },
+            {
+                label: "Awards & Media",
+                link: "/awards-and-media"
+            },
+            {
+                label: "Booking & saledeed",
+                link: "/booking-and-saledeed"
+            },
+        ]
+    },
+    {
+        label: "Projects in Mumbai",
+        data: [
+            {
+                label: "Plot in Thane",
+                link: "/thane"
+            },
+            {
+                label: "Plot in Panvel",
+                link: "/panvel"
+            },
+            {
+                label: "Plot in Khalapur",
+                link: "/khalapur"
+            },
+            {
+                label: "Plot in Ranjanpada",
+                link: "/ranjanpada"
+            },
+            {
+                label: "Plot in Vindhane",
+                link: "/vindhane"
+            },
+            {
+                label: "Plot in Chirle",
+                link: "/chirle"
+            },
+        ]
+    }
+]
