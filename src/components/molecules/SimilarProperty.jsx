c5ad538f7383140a58f71c1613f1ec4e7774e3f5
Akash09082001 <aakashl@letsupgrade.in>
2025-01-18 12:10:01 +0530

    style: similar property & near by locations section ui styled

--- src/components/molecules/SimilarProperty.jsx
+++ src/components/molecules/SimilarProperty.jsx
@@ -1,8 +1,14 @@
 "use client";
-import React from "react";
+
+import { getAllProperty } from "@/actions/property";
+import { formatCurrency } from "@/lib/utils";
+import { IndianRupeeIcon } from "lucide-react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import SectionHeading from "../atoms/SectionHeading";
+import SkeletonCard from "../atoms/SkeletonCard";
+import ImageScroll from "../molecules/ImageScroll";
 import { Button } from "../ui/button";
-import { ArrowRight } from "lucide-react";
 import {
     Carousel,
     CarouselContent,
@@ -10,112 +16,129 @@ import {
     CarouselNext,
     CarouselPrevious,
 } from "../ui/carousel";
-import Autoplay from "embla-carousel-autoplay";
-import Image from "next/image";
 
-const Cards = [
-    {
-        img: "recomonded-property1.jpeg",
-    },
-    {
-        img: "recomonded-property2.jpeg",
-    },
-    {
-        img: "recomonded-property3.jpeg",
-    },
-    {
-        img: "recomonded-property4.jpeg",
-    },
-    {
-        img: "recomonded-property5.jpeg",
-    },
-    {
-        img: "recomonded-property6.jpeg",
-    },
-    {
-        img: "recomonded-property7.jpeg",
-    },
-    {
-        img: "recomonded-property8.jpeg",
-    },
-    {
-        img: "recomonded-property9.jpeg",
-    },
-];
+const Property = () => {
+    const [property, setProperty] = useState([]);
+    const [hoveredIndex, setHoveredIndex] = useState(null);
+    const [loading, setLoading] = useState(false);
 
-const SimilarProperty = () => {
+    const handleMouseEnter = (index) => {
+        setHoveredIndex(index);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredIndex(null);
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            const { result = [] } = await getAllProperty({
+                page: 1,
+                limit: 10,
+            });
+            setProperty(result);
+            setLoading(false);
+        };
+        fetchData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
         <>
             <div className="flex w-full">
                 <div className="flex flex-col gap-4 md:gap-6 w-full max-w-7xl mx-auto px-4 py-6 md:py-8 lg:py-10 xl:py-12">
-                    <div className="flex w-full items-center justify-between">
-                        <h2 className="text-lg font-medium md:text-xl lg:text-2xl">
-                            Similar Property
-                        </h2>
-                        <Link
-                            href={"/"}
-                            className="flex items-center gap-2 hover:text-black text-[#0000FF] text-lg md:text-xl"
-                        >
-                            <span className="hidden md:flex">
-                                See More Properties
-                            </span>
-                            <ArrowRight />
-                        </Link>
-                    </div>
+                    <SectionHeading
+                        title="Similar Property"
+                        link
+                        linkHref="/properties"
+                        linkLabel="See More Properties"
+                    />
                     <div className="flex w-full">
-                        <Carousel
-                            plugins={[
-                                Autoplay({
-                                    delay: 3000,
-                                }),
-                            ]}
-                            opts={{
-                                align: "start",
-                                loop: true,
-                            }}
-                            className="flex w-full h-full"
-                        >
-                            <CarouselContent>
-                                {Cards.map((card, index) => (
-                                    <CarouselItem
-                                        key={`${index}-plot-img`}
-                                        className="sm:basis-1/2 lg:basis-1/3 xl:basis-1/4"
-                                    >
-                                        <div className="flex flex-col w-full border border-neutral-200 rounded-lg group hover:shadow-lg">
-                                            <div className="relative aspect-video bg-cover rounded-t-lg flex w-full overflow-hidden">
-                                                <Image
-                                                    src={`/assets/recommonded-property/${card.img}`}
-                                                    alt="house"
-                                                    fill
-                                                    className="rounded-t-lg group-hover:scale-110 transition-all object-cover"
+                        {loading && property.length === 0 ? (
+                            <SkeletonCard />
+                        ) : (
+                            <Carousel
+                                // plugins={[
+                                //     Autoplay({
+                                //         delay: 3000,
+                                //     })
+                                // ]}
+                                opts={{
+                                    align: "start",
+                                    loop: true,
+                                }}
+                                className="flex w-full h-full"
+                            >
+                                <CarouselContent>
+                                    {property?.map((card, index) => (
+                                        <CarouselItem
+                                            key={`${index}-plot-img`}
+                                            className="basis-44 sm:basis-1/3 lg:basis-1/3 xl:basis-1/4"
+                                        >
+                                            <div
+                                                className="flex flex-col w-full border border-neutral-200 rounded-lg group hover:shadow-lg"
+                                                onMouseEnter={() =>
+                                                    handleMouseEnter(index)
+                                                }
+                                                onMouseLeave={handleMouseLeave}
+                                            >
+                                                <ImageScroll
+                                                    card={card}
+                                                    isHovered={
+                                                        hoveredIndex === index
+                                                    }
                                                 />
-                                            </div>
-                                            <div className=" p-4 space-y-2">
-                                                <div className="w-full">
-                                                    <span className="text-sm">
-                                                        1 BHK Flat
+                                                <div className="p-2 sm:p-4 relative space-y-2">
+                                                    <div className="w-full">
+                                                        <span className="text-xs sm:text-sm">
+                                                            {card.propertyType}
+                                                        </span>
+                                                        <h2 className="text-sm sm:text-base flex items-center font-medium">
+                                                            <IndianRupeeIcon className="!size-4" />{" "}
+                                                            {formatCurrency(
+                                                                card.priceTotal
+                                                            )}
+                                                            |{" "}
+                                                            {
+                                                                card.plotArea
+                                                                    ?.value
+                                                            }{" "}
+                                                            {
+                                                                card.plotArea
+                                                                    ?.unit
+                                                            }
+                                                        </h2>
+                                                        <p className="text-gray-600 gap-2 flex text-xs sm:text-sm">
+                                                            {card.locality}
+                                                            ,&nbsp;
+                                                            {card.city}
+                                                        </p>
+                                                    </div>
+                                                    <span className="hidden md:flex">
+                                                        Ready Move
                                                     </span>
-                                                    <h2 className="text-lg font-medium">
-                                                        &#8377; 30 Lac | 1000
-                                                        sqft
-                                                    </h2>
+                                                    <div className="flex w-full md:absolute md:px-4 pb-1 sm:pb-3 md:left-0 md:-bottom-40 md:group-hover:bottom-0 md:transition-all">
+                                                        <Button
+                                                            asChild
+                                                            className="w-full text-xs sm:text-sm h-[unset] rounded-full py-1 sm:py-1.5 bg-[#0000FF]"
+                                                        >
+                                                            <Link
+                                                                href={`/properties/${card.propertyId}`}
+                                                            >
+                                                                View Details
+                                                            </Link>
+                                                        </Button>
+                                                    </div>
                                                 </div>
-                                                <div className="flex flex-col space-y-1 w-full">
-                                                    <p className="text-gray-600 text-sm">
-                                                        Vinay Nagar, Mumbai
-                                                    </p>
-                                                </div>
-                                                <Button className="rounded-full bg-[#0000FF]">
-                                                    View Details
-                                                </Button>
                                             </div>
-                                        </div>
-                                    </CarouselItem>
-                                ))}
-                            </CarouselContent>
-                            <CarouselPrevious className="-left-3 sm:left-0 lg:-left-8" />
-                            <CarouselNext className="-right-3 sm:right-0 lg:-right-8" />
-                        </Carousel>
+                                        </CarouselItem>
+                                    ))}
+                                </CarouselContent>
+                                <CarouselPrevious className="-left-3 sm:left-0 lg:-left-8" />
+                                <CarouselNext className="-right-3 sm:right-0 lg:-right-8" />
+                            </Carousel>
+                        )}
                     </div>
                 </div>
             </div>
@@ -123,4 +146,4 @@ const SimilarProperty = () => {
     );
 };
 
-export default SimilarProperty;
+export default Property;
