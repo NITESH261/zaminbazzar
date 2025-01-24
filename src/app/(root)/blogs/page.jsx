465b59095cf21efa6bac6ab0c1e33ca8f7e76420
Akash Lakade <akashlakade130@gmail.com>
2025-01-24 22:39:25 +0530

    refactor: blog summary sliced

--- src/app/(root)/blogs/page.jsx
+++ src/app/(root)/blogs/page.jsx
@@ -11,28 +11,31 @@ const BlogPage = () => {
         {
             id: 1,
             title: "Mahamumbai - Your Future Investment Opportunity",
-            excerpt: "MAHAMUMBAI enjoys high property supply, demand is growing due to construction of upcoming sea-link from Sewri to Nhava Sheva, Navi Mumbai Airport, and proximity to the railway station and national highways around. Also, this area benefits from its proximity to prominent commercial parts of the city such as Kopar Khairane, Taloja, and Airoli.",
+            excerpt:
+                "MAHAMUMBAI enjoys high property supply, demand is growing due to construction of upcoming sea-link from Sewri to Nhava Sheva, Navi Mumbai Airport, and proximity to the railway station and national highways around. Also, this area benefits from its proximity to prominent commercial parts of the city such as Kopar Khairane, Taloja, and Airoli.",
             image: "/assets/blog/1.png",
         },
         {
             id: 2,
             title: "Why Thane is good for investment in Plots?",
-            excerpt: "Thane is an emerging cosmopolitan city in the Mumbai Metropolitan Area and the most preferred real estate investment destination. Affordable lifestyle with necessary amenities at value-oriented pricing and Infrastructural development has been the key differentiation factors that set Thane apart from the other real estate markets in the nearby adjoining areas",
+            excerpt:
+                "Thane is an emerging cosmopolitan city in the Mumbai Metropolitan Area and the most preferred real estate investment destination. Affordable lifestyle with necessary amenities at value-oriented pricing and Infrastructural development has been the key differentiation factors that set Thane apart from the other real estate markets in the nearby adjoining areas",
             image: "/assets/blog/2.jpg",
         },
         {
             id: 3,
             title: "The Dos and Don’ts of Buying Land",
-            excerpt: "Buying land is a dream for many people. Purchasing land is also one of the important investments one can make. The Dos and Don’ts of Buying Land Purchasing a plot tailored for your dream home may seem like the perfect investment. Whether you are purchasing a home, ranch property, or farm, there are things to consider that may prevent you from making your land purchase a success.",
+            excerpt:
+                "Buying land is a dream for many people. Purchasing land is also one of the important investments one can make. The Dos and Don’ts of Buying Land Purchasing a plot tailored for your dream home may seem like the perfect investment. Whether you are purchasing a home, ranch property, or farm, there are things to consider that may prevent you from making your land purchase a success.",
             image: "/assets/blog/3.webp",
         },
         {
             id: 4,
             title: "Advantages of owning a Plots.",
-            excerpt: "There is no doubt that real estate is one of the most fruitful and stable forms of investment. While residential homes have become almost synonymous with real estate investments, they aren’t the only option available. If you are considering making a real estate investment, here’s why purchasing a plot of land should top your list.",
+            excerpt:
+                "There is no doubt that real estate is one of the most fruitful and stable forms of investment. While residential homes have become almost synonymous with real estate investments, they aren’t the only option available. If you are considering making a real estate investment, here’s why purchasing a plot of land should top your list.",
             image: "/assets/blog/4.jpg",
         },
-
     ];
 
     const [page, setPage] = useState(1);
@@ -83,7 +86,10 @@ const BlogPage = () => {
                                     <p className="text-sm text-gray-600">
                                         {blog.excerpt}
                                     </p>
-                                    <Button asChild className="mt-2 w-full bg-[#0000ff] text-white rounded-full py-1.5 text-sm">
+                                    <Button
+                                        asChild
+                                        className="mt-2 w-full bg-[#0000ff] text-white rounded-full py-1.5 text-sm"
+                                    >
                                         <Link href={`/blogs/${blog.id}`}>
                                             Read More
                                         </Link>
