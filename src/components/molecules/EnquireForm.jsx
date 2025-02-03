39a072f26b1a1c1e908b1a812b03886a364bc1d3
Akash Lakade <akashlakade130@gmail.com>
2025-02-04 00:43:19 +0530

    refactor: enquiry form api updated

--- src/components/molecules/EnquireForm.jsx
+++ src/components/molecules/EnquireForm.jsx
@@ -1,20 +1,22 @@
-"use client"
+"use client";
 
-import { createPropertyEnquiry } from "@/actions/property"
-import { cn } from "@/lib/utils"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { format } from "date-fns"
-import { CalendarIcon } from "lucide-react"
-import { useState } from "react"
-import { useForm } from "react-hook-form"
-import { toast } from "sonner"
-import { z } from "zod"
-import Loading from "../atoms/Loading"
-import { Button } from "../ui/button"
-import { Calendar } from "../ui/calendar"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
-import { Input } from "../ui/input"
-import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover"
+import { createPropertyEnquiry } from "@/actions/property";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+import Loading from "../atoms/Loading";
+import { Button } from "../ui/button";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
 
 const formSchema = z.object({
     name: z.string().min(2, {
@@ -24,27 +26,23 @@ const formSchema = z.object({
         message: "Mobile No must be at least 10 Digit.",
     }),
     email: z.string().email(),
-    visitBy: z.coerce.date({
-        required_error: "Date is required.",
-    }),
-})
+});
 
 const EnquireForm = ({ propertyId, uid }) => {
-
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(false);
 
     const form = useForm({
         resolver: zodResolver(formSchema),
         defaultValues: {},
-    })
+    });
 
     const onSubmit = async (values) => {
-        setLoading(true)
+        setLoading(true);
         try {
-            let body = { ...values, uid }
-            const resp = await createPropertyEnquiry({ body, propertyId })
-            setLoading(false)
-            form.reset()
+            let body = { ...values, uid };
+            const resp = await createPropertyEnquiry({ body, propertyId });
+            setLoading(false);
+            form.reset();
             toast.success(resp.message);
         } catch (error) {
             setLoading(false);
@@ -55,7 +53,10 @@ const EnquireForm = ({ propertyId, uid }) => {
     return (
         <div className="w-full">
             <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-3"
+                >
                     <FormField
                         control={form.control}
                         name="name"
@@ -66,9 +67,7 @@ const EnquireForm = ({ propertyId, uid }) => {
                                     <Input
                                         placeholder="Enter your fullname"
                                         {...field}
-                                        value={
-                                            field.value || ""
-                                        }
+                                        value={field.value || ""}
                                     />
                                 </FormControl>
                                 <FormMessage />
@@ -85,9 +84,7 @@ const EnquireForm = ({ propertyId, uid }) => {
                                     <Input
                                         placeholder="Enter your Email"
                                         {...field}
-                                        value={
-                                            field.value || ""
-                                        }
+                                        value={field.value || ""}
                                     />
                                 </FormControl>
                                 <FormMessage />
@@ -104,60 +101,9 @@ const EnquireForm = ({ propertyId, uid }) => {
                                     <Input
                                         placeholder="Enter your mobile no."
                                         {...field}
-                                        value={
-                                            field.value || ""
-                                        }
+                                        value={field.value || ""}
                                     />
                                 </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="visitBy"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>
-                                    Schedule Site Visit
-                                </FormLabel>
-                                <Popover>
-                                    <PopoverTrigger asChild>
-                                        <FormControl>
-                                            <Button
-                                                variant={"outline"}
-                                                className={cn(
-                                                    "w-full pl-3 text-left font-normal",
-                                                    !field.value &&
-                                                    "text-muted-foreground"
-                                                )}
-                                            >
-                                                {field.value ? (
-                                                    format(
-                                                        field.value,
-                                                        "PP"
-                                                    )
-                                                ) : (
-                                                    <span>
-                                                        Select Date
-                                                    </span>
-                                                )}
-                                                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                                            </Button>
-                                        </FormControl>
-                                    </PopoverTrigger>
-                                    <PopoverContent
-                                        className="w-auto p-0"
-                                        align="start"
-                                    >
-                                        <Calendar
-                                            mode="single"
-                                            selected={field.value}
-                                            onSelect={field.onChange}
-                                            initialFocus
-                                        />
-                                    </PopoverContent>
-                                </Popover>
                                 <FormMessage />
                             </FormItem>
                         )}
@@ -168,7 +114,7 @@ const EnquireForm = ({ propertyId, uid }) => {
                 </form>
             </Form>
         </div>
-    )
-}
+    );
+};
 
-export default EnquireForm
+export default EnquireForm;
--- src/components/organism/SendEnquiry.jsx
+++ src/components/organism/SendEnquiry.jsx
@@ -1,11 +1,7 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
 import Autoplay from "embla-carousel-autoplay";
 import Image from "next/image";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { Button } from "../ui/button";
 import {
     Carousel,
     CarouselContent,
@@ -13,32 +9,9 @@ import {
     CarouselNext,
     CarouselPrevious,
 } from "../ui/carousel";
-import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "../ui/form";
-import { Input } from "../ui/input";
-import { Textarea } from "../ui/textarea";
+import EnquireForm from "../molecules/EnquireForm";
 
-const formSchema = z.object({
-    username: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
-    }),
-});
-
-const SendEnquiry = ({ cards }) => {
-    const form = useForm({
-        resolver: zodResolver(formSchema),
-        defaultValues: {},
-    });
-
-    function onSubmit(values) {
-        console.log(values);
-    }
+const SendEnquiry = ({ cards, propertyId, uid }) => {
     return (
         <div className="flex flex-col gap-4 md:gap-6 lg:gap-8 xl:gap-10 px-4 py-6 md:py-8 lg:py-10 w-full max-w-[1360px] border border-neutral-200 shadow-md mx-auto rounded-3xl h-auto relative bg-white">
             <div className="flex w-full items-center justify-center">
@@ -48,80 +21,7 @@ const SendEnquiry = ({ cards }) => {
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 md:gap-4 bg-white w-full max-w-6xl mx-auto border border-neutral-200 rounded-2xl">
                 <div className="flex flex-col space-y-2 items-center justify-center w-full p-6">
-                    <Form {...form}>
-                        <form
-                            onSubmit={form.handleSubmit(onSubmit)}
-                            className="space-y-3 w-full"
-                        >
-                            <FormField
-                                control={form.control}
-                                name="fullname"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Fullname</FormLabel>
-                                        <FormControl>
-                                            <Input
-                                                placeholder="Enter your fullname"
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name="email"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Email</FormLabel>
-                                        <FormControl>
-                                            <Input
-                                                placeholder="Enter your email"
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name="mobile"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Mobile</FormLabel>
-                                        <FormControl>
-                                            <Input
-                                                placeholder="Enter your mobile no."
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name="message"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Message</FormLabel>
-                                        <FormControl>
-                                            <Input
-                                                placeholder="Enter your message"
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                            <Button className="rounded-3xl w-full bg-[#581a95]">
-                                Send Enquiry
-                            </Button>
-                        </form>
-                    </Form>
+                    <EnquireForm propertyId={propertyId} uid={uid} />
                 </div>
                 <div className="flex w-full xl:col-span-2">
                     <Carousel
