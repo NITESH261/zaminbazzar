db6f5e1f1ccb2cda7bd1631463b2bfddf4712e63
vinay1601 <vinayakarjun1234@gmail.com>
2025-02-28 21:26:35 +0530

    add thank you page

---
+++ src/app/(root)/thank-you/page.jsx
@@ -0,0 +1,14 @@
+import Footer from '@/components/organism/Footer'
+import Thankyouimg from '@/components/organism/thankyou/Thankyouimg.jsx'
+import React from 'react'
+
+const page = () => {
+    return (
+        <>
+            <Thankyouimg />
+            <Footer />
+        </>
+    )
+}
+
+export default page
---
+++ src/components/organism/thankyou/Thankyouimg.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { cn } from "@/lib/utils";
+
+const Thankyouimg = () => {
+    return (
+        <>
+            <div
+                className={cn(
+                    "flex w-full min-h-screen  bg-[url('/assets/thank/1.webp')] bg-no-repeat bg-cover bg-center",
+
+                )}
+            ></div>
+        </>
+    )
+}
+
+export default Thankyouimg
