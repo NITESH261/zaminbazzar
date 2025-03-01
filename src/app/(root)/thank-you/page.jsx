0abdb406a6329fbdb10352b6a35a66d305f9bcd1
Akash Lakade <akashlakade130@gmail.com>
2025-03-01 22:26:05 +0530

    refactor: thankyou page correction done

--- src/app/(root)/thank-you/page.jsx
+++ src/app/(root)/thank-you/page.jsx
@@ -1,26 +1,34 @@
-import Footer from '@/components/organism/Footer'
-import Image from 'next/image'
-import { cn } from "@/lib/utils";
-import React from 'react'
+import Footer from "@/components/organism/Footer";
+import Image from "next/image";
+import React from "react";
 
 const page = () => {
     return (
         <>
-            <div className='flex w-full h-screen'>
-                <div className='flex flex-col w-full max-w-7xl items-center justify-center mx-auto'>
+            <div className="flex w-full h-[calc(100vh-70px)]">
+                <div className="flex flex-col w-full max-w-7xl items-center justify-center mx-auto">
                     <div className="flex aspect-square w-52 relative">
-                        <Image src={"/assets/thank/thank.png"} alt='thank-you-logo' fill className='object-contain' />
+                        <Image
+                            src={"/assets/thank/thank.png"}
+                            alt="thank-you-logo"
+                            fill
+                            className="object-contain"
+                        />
                     </div>
-                    <div className='flex w-full text-center gap-4 flex-col'>
-                        <h1 className='text-5xl font-extrabold text-[#6f272b]'>Thank You</h1>
-                        <p className='text-center text-base'>We have received your enquiry. Our team will get back to you soon.</p>
+                    <div className="flex w-full text-center gap-4 flex-col">
+                        <h1 className="text-5xl font-extrabold text-[#6f272b]">
+                            Thank You
+                        </h1>
+                        <p className="text-center text-base">
+                            We have received your enquiry. Our team will get
+                            back to you soon.
+                        </p>
                     </div>
                 </div>
             </div>
             <Footer />
         </>
-    )
-}
+    );
+};
 
-export default page
-
+export default page;
