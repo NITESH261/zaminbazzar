a513e91bf37d0bb5cf6e103d5077992d6fab1ce9
NITESH261 <niteshvshukla56@gmail.com>
2025-06-20 15:42:32 +0530

    subs plan

--- src/app/(root)/privacy-policy copy/page.jsx
+++ src/app/(root)/privacy-policy copy/page.jsx
@@ -1,10 +1,12 @@
 import Footer from "@/components/organism/Footer";
 import PrivacyPolicy from "@/components/organism/Privacy-Policy/PrivacyPolicy";
+import WhyChooseZaminWale from "@/components/organism/Privacy-Policy/WhyChooseZaminWale";
 import React from "react";
 
 const page = () => {
     return (
         <>
+            <WhyChooseZaminWale/>
             <PrivacyPolicy/>
             <Footer />
         </>
