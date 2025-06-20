9dae859ac3a870b6a5435b6b5bfd5c62397a7302
NITESH261 <niteshvshukla56@gmail.com>
2025-06-20 13:42:37 +0530

    Dealer

--- src/app/(root)/dealer/page.jsx
+++ src/app/(root)/dealer/page.jsx
@@ -1,13 +1,13 @@
 import Footer from "@/components/organism/Footer";
-import whyc from "@/components/organism/Dealer/whyc";
-import subscriptionplan from "@/components/organism/Dealer/subscriptionplan";
+import Whyc from "@/components/organism/Dealer/whyc";
+import SubscriptionPlan from "@/components/organism/Dealer/subscriptionplan";
 import React from "react";
 
 const page = () => {
     return (
         <>
-            <whyc/>
-            <subscriptionplan/>
+            <Whyc/>
+            <Subscriptionplan/>
             <Footer />
         </>
     );
