daeb5a5a80f005fd475abc0b90301ff9d119986c
NITESH261 <niteshvshukla56@gmail.com>
2025-06-22 13:16:30 +0530

    faq

--- src/app/(root)/privacy-policy copy/page.jsx
+++ src/app/(root)/privacy-policy copy/page.jsx
@@ -2,6 +2,7 @@ import Footer from "@/components/organism/Footer";
 import WhyChooseZaminWale from "@/components/organism/Privacy-Policy copy/WhyChooseZaminWale";
 import SubscriptionPlans from "@/components/organism/Privacy-Policy copy/SubscriptionPlans";
 import ClientTestimonials from "@/components/organism/Privacy-Policy copy/ClientTestimonials";
+import FAQ from "@/components/organism/Privacy-Policy copy/FAQ";
 import React from "react";
 
 const page = () => {
@@ -10,6 +11,7 @@ const page = () => {
             <WhyChooseZaminWale/>
             <SubscriptionPlans/>
             <ClientTestimonials/>
+            <FAQ/>
             <Footer />
         </>
     );
