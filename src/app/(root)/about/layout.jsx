d19870a575e504c0ec771bb950797373c0a082c4
NITESH261 <niteshvshukla56@gmail.com>
2025-09-16 17:49:01 +0530

    seo change

--- src/app/(root)/about/layout.jsx
+++ src/app/(root)/about/layout.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 export const metadata = {
-    title: "Zaminwale – Buying and Selling of Land",
+    title: "Zaminbazzar – Buying and Selling of Land",
     description:
-        "Zaminwale.com is India's leading online platform for buying and selling land. Find verified listings, trusted sellers, and the best real estate deals.",
+        "Zaminbazzar.com is India's leading online platform for buying and selling land. Find verified listings, trusted sellers, and the best real estate deals.",
     keywords: [
         "buy land",
         "sell land",
@@ -13,27 +13,27 @@ export const metadata = {
         "farmland",
         "commercial land",
         "residential plots",
-        "Zaminwale",
+        "Zaminbazzar",
     ],
     openGraph: {
-        title: "Zaminwale – Buying and Selling of Land",
+        title: "Zaminbazzar – Buying and Selling of Land",
         description:
-            "Zaminwale.com is India's leading online platform for buying and selling land. Find verified listings, trusted sellers, and the best real estate deals.",
+            "Zaminbazzar.com is India's leading online platform for buying and selling land. Find verified listings, trusted sellers, and the best real estate deals.",
         siteName: "LetsUpgrade",
         images: [
             {
                 url: "https://zaminwale-private.s3.ap-south-1.amazonaws.com/assets/property/images/f5eab6fc-6526-4a31-bfa3-2f1acc4ea435.png",
                 width: 1200,
                 height: 628,
-                alt: "Zaminwale",
+                alt: "Zaminbazzar",
             },
         ],
     },
     twitter: {
         card: "summary_large_image",
-        title: "Zaminwale – Buying and Selling of Land",
+        title: "Zaminbazzar – Buying and Selling of Land",
         description:
-            "Zaminwale.com is India's leading online platform for buying and selling land. Find verified listings, trusted sellers, and the best real estate deals.",
+            "Zaminbazzar.com is India's leading online platform for buying and selling land. Find verified listings, trusted sellers, and the best real estate deals.",
         images: [
             "https://zaminwale-private.s3.ap-south-1.amazonaws.com/assets/property/images/f5eab6fc-6526-4a31-bfa3-2f1acc4ea435.png",
         ],
